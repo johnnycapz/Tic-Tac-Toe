8889f5febb098acf91e3bd95d7d7caf63719f12d
John C <johnacapuano@gmail.com>
2026-05-21 07:13:59 -0400

    added more functionality

--- main.js
+++ main.js
@@ -27,14 +27,47 @@ function pickSpot(index, item) {
 
 function evaluateBoard() {
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf#finding_all_the_occurrences_of_an_element
-    const indices = [];
+    const indicesOfX = [];
+    const indicesOfO = [];
     console.log(gameboard.gameboardArray);
-    let indexArray = gameboard.gameboardArray.indexOf("x");
-    while (indexArray !== -1) {
-        indices.push(indexArray);
-        indexArray = gameboard.gameboardArray.indexOf("x", indexArray + 1);
+    let indexArrayX = gameboard.gameboardArray.indexOf("x");
+    while (indexArrayX !== -1) {
+        indicesOfX.push(indexArrayX);
+        indexArrayX = gameboard.gameboardArray.indexOf("x", indexArrayX + 1);
     }
-    console.log(indices);
+
+    let indexArrayO = gameboard.gameboardArray.indexOf("o");
+    while (indexArrayO !== -1) {
+        indicesOfO.push(indexArrayO);
+        indexArrayO = gameboard.gameboardArray.indexOf("o", indexArrayO + 1);
+    }
+    console.log(indicesOfX);
+    console.log(indicesOfO);
     // [0, 2, 4]
+    const winningArray1 = [0 , 1 ,2];
+    const winningArray2 = [3 , 4 ,5];
+    const winningArray3 = [6 , 7 ,8];
+    const winningArray4 = [0 , 3 ,6];
+    const winningArray5 = [1 , 4 ,7];
+    const winningArray6 = [2 , 5 ,8];
+    const winningArray7 = [0 , 4 ,8];
+    const winningArray8 = [2 , 4 , 6];
+    //https://www.freecodecamp.org/news/how-to-compare-arrays-in-javascript/ 
+    if ((JSON.stringify(indicesOfX) === JSON.stringify(winningArray1))|| (JSON.stringify(indicesOfX) === JSON.stringify(winningArray2)) || (JSON.stringify(indicesOfX) === JSON.stringify(winningArray3)) || (JSON.stringify(indicesOfX) === JSON.stringify(winningArray4)) || (JSON.stringify(indicesOfX) === JSON.stringify(winningArray5)) || (JSON.stringify(indicesOfX) === JSON.stringify(winningArray6)) || (JSON.stringify(indicesOfX) === JSON.stringify(winningArray7)) || (JSON.stringify(indicesOfX) === JSON.stringify(winningArray8))) {
+        console.log("Player X Wins");
+    } else if ((JSON.stringify(indicesOfO) === JSON.stringify(winningArray1))|| (JSON.stringify(indicesOfO) === JSON.stringify(winningArray2)) || (JSON.stringify(indicesOfO) === JSON.stringify(winningArray3)) || (JSON.stringify(indicesOfO) === JSON.stringify(winningArray4)) || (JSON.stringify(indicesOfO) === JSON.stringify(winningArray5)) || (JSON.stringify(indicesOfO) === JSON.stringify(winningArray6)) || (JSON.stringify(indicesOfO) === JSON.stringify(winningArray7)) || (JSON.stringify(indicesOfO) === JSON.stringify(winningArray8))) {
+        console.log("Player O Wins");
+    } else {
+        console.log("no one wins");
+    }
 
     }
+
+
+pickSpot(0, "x")
+pickSpot(1, "o")
+pickSpot(3, "x")
+pickSpot(4, "o")
+pickSpot(6, "x")
+
+evaluateBoard();
